afe3ed3b1690a3352c4cafa41f37643bf9ed7a6d
encorehu <huyoo353@126.com>
2014-04-17 15:23:47 +0800

    add urls

--- urls.py
+++ urls.py
@@ -4,4 +4,9 @@ from handlers.index import IndexHandler
 url_patterns = [
     (r"/", IndexHandler),
     (r"/foo", FooHandler),
+    (r"/mystats", FooHandler),
+    (r"/blocks", FooHandler),
+    (r"/topcontributors", FooHandler),
+    (r"/userstats", FooHandler),
+    (r"/userstats/\w*", FooHandler),
 ]

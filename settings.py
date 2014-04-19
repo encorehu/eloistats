bade52e3b836e5290c5cd714e95e35da35917db5
encorehu <huyoo353@126.com>
2014-04-19 21:55:45 +0800

    add ui_methods.py for template render

--- settings.py
+++ settings.py
@@ -38,12 +38,15 @@ if 'DEPLOYMENT_TYPE' in os.environ:
 else:
     DEPLOYMENT = DeploymentType.SOLO
 
+import ui_methods
+
 settings = {}
 settings['debug'] = DEPLOYMENT != DeploymentType.PRODUCTION or options.debug
 settings['static_path'] = MEDIA_ROOT
 settings['cookie_secret'] = "your-cookie-secret"
 settings['xsrf_cookies'] = True
 settings['template_loader'] = tornado.template.Loader(TEMPLATE_ROOT)
+settings['ui_methods'] = ui_methods
 
 SYSLOG_TAG = "boilerplate"
 SYSLOG_FACILITY = logging.handlers.SysLogHandler.LOG_LOCAL2
---
+++ ui_methods.py
@@ -0,0 +1,4 @@
+
+# http://stackoverflow.com/questions/12993835/passing-a-custom-python-function-into-a-tornado-template
+def slice(s, length):
+    return s[0:length]
